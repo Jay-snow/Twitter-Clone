a55c91324b3a6d2e9e6f3fab862f93bf78ea254b
Jay-snow <marcusjsnow1@gmail.com>
2019-03-12 13:21:10 -0400

    Finally! 'Lisa' is the first account with a secured hash password. All previous accounts no longer work as they were not properly stored with hash. Upon login now checks password with password_verify().

--- scripts/signin.php
+++ scripts/signin.php
@@ -23,16 +23,52 @@ if (!empty($username)) {
         $sql = "SELECT * FROM users WHERE username = '$username'";
         $result = mysqli_query($conn,$sql);
         $row = mysqli_fetch_array($result);
+      //  $passwordHash = password_hash('password', PASSWORD_DEFAULT);
+
+       //   var_dump($row['username']);
+       //   var_dump($row['password']);
+       //   echo $password;
 
 
-          var_dump($row['username']);
-          var_dump($row['password']);
-          echo $password;
+          
         // If the query returns null
         if (isset($row['username'])  ) {
           
+          echo "\n User name verified\n \n";
+          echo "<br>";
+          
+           $passwordHash = $row['password'];
+          $hashPost = password_hash($_POST['password'],PASSWORD_DEFAULT);
 
-          if ($row['password'] == $password) {
+
+          echo "\r\n The Password not hashed: ";
+          echo $_POST['password'];
+
+          echo "<br>";
+          echo 'The Password Hashed: ';
+          echo $hashPost;
+
+          echo "<br>";
+          echo 'The Real Hash: ';
+          echo $row['password'];
+
+          $localHash = '$2y$10$gFvw5TBD9HZSOTIZfELJK.IUyBRO35Gg0QHZWiNlv3CR8wfMf2AG6';
+
+          echo "<br> Does Password Validate: ";
+          $istrue = password_verify($_POST['password'], $hashPost);
+          var_dump($istrue);
+          echo "<br> Local Hash: ";
+          var_dump($localHash);
+          echo "<br> Actual Stored Password/Hash: ";
+          var_dump($row['password']);
+          echo "<br> New Hash from input: ";
+          var_dump($hashPost);
+
+          
+
+          if ( password_verify($_POST['password'], $passwordHash) ) {
+
+            
 
             $_SESSION["username"] = $username;
           $_SESSION["loggedin"] = true;
