4c7dfa5ec21ad49eecbf58006c5588d2f9e332b9
Jay-snow <marcusjsnow1@gmail.com>
2019-03-20 17:29:23 -0400

    Big strategic change: Ignoring the AJAX call for the timeline for now. This project was originally to learn PHP, and I've wasted a lot of time trying to get this AJAX call to work the way I want it to. I was also able to make some changes to my HTML that removed the need for a few of these listeners. I will loop back around to having the timeline update without refreshing at another point.

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,6 +2,10 @@
 
 //LOGOUT FUNCTION
 //AJAX call to request PHP logout. I -think- this is slightly faster than directing to the PHP file, because it says 1 page from being loaded.
+
+
+
+
 (function() {
   var httpRequest;
   //Adds a listener to the logout
@@ -97,47 +101,47 @@ function newPost() {
 //AJAX call to request PHP logout. I -think- this is slightly faster than directing to the PHP file, because it says 1 page from being loaded.
 
 
-(function() {
-  var httpRequest;
-  //Adds a listener to the logout
-  var postButton = document.getElementById("newPost");
-  postButton.addEventListener('click', makeRequest);
+// (function() {
+//   var httpRequest;
+//   //Adds a listener to the logout
+//   var postButton = document.getElementById("newPost");
+//   postButton.addEventListener('click', makeRequest);
 
   
 
-  //Makes the request from the server
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
+//   //Makes the request from the server
+//   function makeRequest() {
+//     httpRequest = new XMLHttpRequest();
 
-    if (!httpRequest) {
-      alert('Giving up, Cannot create instance');
-      return false;
-    }
-    //Execution of request is passed to alertContents
-    httpRequest.onreadystatechange = alertContents;
-    //This script should pass post data into database
-    httpRequest.open('POST', './scripts/newPost.php');
-    httpRequest.send();
+//     if (!httpRequest) {
+//       alert('Giving up, Cannot create instance');
+//       return false;
+//     }
+//     //Execution of request is passed to alertContents
+//     httpRequest.onreadystatechange = alertContents;
+//     //This script should pass post data into database
+//     httpRequest.open('POST', './scripts/newPost.php');
+//     httpRequest.send();
     
     
-  }
-    //Checks is response was recieved OK, if not, throw an error.
-    //In case it can't reach the server, show a description why with try catch
-    function alertContents() {
+//   }
+//     //Checks is response was recieved OK, if not, throw an error.
+//     //In case it can't reach the server, show a description why with try catch
+//     function alertContents() {
      
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-            console.log("The request was successful!");
-            newPost();
-            alert("TEST2");
+//         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//           if (httpRequest.status === 200) {
+//             console.log("The request was successful!");
+//             newPost();
+//             alert("TEST2");
 
            
 
-          } else {
-            console.log('There was a problem with the request.');
-          }
-        }
+//           } else {
+//             console.log('There was a problem with the request.');
+//           }
+//         }
 
-    }
+//     }
   
-})();
+// })();
