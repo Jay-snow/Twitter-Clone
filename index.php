b03ad256cc6fcf56314eacf50e270f93ef84065a
Jay-snow <marcusjsnow1@gmail.com>
2019-03-28 13:12:37 -0400

    Wrote a better introduction, changed container to fluid.

--- index.php
+++ index.php
@@ -20,21 +20,22 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
 
 
 ?>
-  <body class="text-center">
-  <div class="container">
+  <body >
+  <div class="container-fluid">
   <div class="row">
   <div class="col-12">
-<h1> OctoBit </h1>  <i class="nes-mario m-4"></i>
+<!-- <h1> OctoBit </h1>  <i class="nes-mario m-4"></i> -->
   </div>
   
 
   <section class="col col-md-5">
 
   <div class="nes-container is-dark with-title">
-  <p class="title">Welcome to Octo Bit?</p>
-  <p>OctoBit is a twitter-clone I created to teach myself PHP.</p>
-  <p>The amazing CSS framework was created by <a href="https://nostalgic-css.github.io/NES.css/">B.C.Rikko, creator of NES.CSS</a>. You should check it out! </p>
-  <p> Used Tools: <a href="https://nostalgic-css.github.io/NES.css/">NES.CSS</a>, Javascript, PHP, MySQL, Bootstrap 4  </p>
+  <p class="title">Welcome to Octo Bit!</p>
+  <i class="nes-kirby d-block"></i>
+  <p>OctoBit is an 8-bit social media platform, inspired by Twitter. </p> <p> There's no e-mail or validation needed - just create an account and get started! </p>
+  <p>The amazing CSS framework was created by <a href="https://nostalgic-css.github.io/NES.css/">B.C.Rikko, creator of NES.CSS</a>. </p>
+  <p> This website was created just for fun to teach myself PHP and mySQL. </p>
 </div>
 
 
@@ -42,26 +43,22 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
 
   <section  class="col col-md-5 ">
     <!-- Login Form -->
-    <form  action="scripts/signin.php" class="form-signin" method="POST">
+    <form  action="scripts/signin.php" class="form-signin p-3" method="POST">
       
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <h1 class="h3 mb-3 font-weight-normal"># Login</h1>
       
       <!-- Username -->
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input name="username" id="inputEmail" class="form-control" placeholder="Email address"  autofocus>
+      <label for="inputEmail" class="sr-only">Username</label>
+      <input name="username" id="inputEmail" class="form-control" placeholder="Username"  autofocus>
 
       <!--Password -->
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" >
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
+
 
       <!-- Buttons -->
-      <button class="nes-btn is-primary" name="submit" type="submit">Sign in</button>
-      <a class="nes-btn is-warning" href="create-account.php" >Create New Account</a>
+      <button class="nes-btn is-primary mt-3" name="submit" type="submit">Log in</button>
+      <a class="nes-btn is-warning mt-3" href="create-account.php" >Create New Account</a>
 
       <!-- Credits -->
       <!-- <p class="mt-5 mb-3 text-muted">Created by Marcus Snow</p>
